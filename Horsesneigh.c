c2a6b2a2a63454f78e2ca1ec7b4ce04a80aaee14
ragalayaswara <prathima.u@gmail.com>
2022-10-14 23:54:47 -0700

    Update Horsesneigh.c

--- Horsesneigh.c
+++ Horsesneigh.c
@@ -1,10 +1,22 @@
-/******************************************************************************
+/* In real world horses neigh, and you can count them by listening to them.
 
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
+For this problem you will be given an input string consisting of lowercases letters which represents combination of neigh of different horses.
 
-*******************************************************************************/
+You need to return an integer corresponding to minimum number of distinct horses which can produce the given sequence.
+
+If the input string is not a combination of valid neigh from different horses return -1.
+
+Example :
+
+Input : "nei"
+Output : -1
+Explanation: Not a valid neigh.
+Input : "neighneigh"
+Output : 1
+Explanation: Single horse yelling neigh two times.
+Input : "neingeighh"
+Output : 2
+Explanation: Second horse can be seen speaking before the first one finished. */
 
 #include <stdio.h>
 #include <string.h>
@@ -41,7 +53,6 @@ int counthorses(char s[])
     int freq[26]={0};
     int no_of_horses=0;
     int answer=0;
-    //char c[20]="iiinnnn";
     int i=0;
     while(s[i] != '\0')
     {
